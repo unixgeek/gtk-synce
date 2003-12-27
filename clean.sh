81b2e9caeaf20b27fd830082d5f275c9a29783b6
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2003-12-27 16:45:53 +0000

    Remove uneeded files.

--- clean.sh
+++ clean.sh
@@ -2,6 +2,9 @@
 
 make clean
 rm -f config.status config.cache config.log configure Makefile.in Makefile aclocal.m4 confdefs.h
+rm -f gtk-synce.spec
+rm -f build_rpm.sh
+rm -f build_tgz.sh
 rm -f src/Makefile.in
 rm -f src/Makefile
 rm -fr src/.deps
